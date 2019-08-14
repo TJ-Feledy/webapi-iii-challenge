bceda2f644a170d5d6dfd7a2c139a909090da1f5
teej387 <teej387@yahoo.com>
2019-08-13 23:56:33 -0500

    Created validatePost() middleware for userRouter.js

--- users/userRouter.js
+++ users/userRouter.js
@@ -64,7 +64,14 @@ function validateUser(req, res, next) {
 }
 
 function validatePost(req, res, next) {
-
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: 'missing user data' })
+  }
+  else if (!req.body.text || req.body.text.split('').length === 0) {
+    res.status(400).json({ message: 'missing required name field' })
+  } else {
+    next()
+  }
 };
 
 module.exports = router;
